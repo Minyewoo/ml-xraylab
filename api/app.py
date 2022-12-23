db5533b5971222128de14da8d8e4f35d6ad9b113
kchpomp <d.maltsev.tmn@gmail.com>
2022-12-23 13:09:22 +0300

    Changed login endpoint: email instead of username

--- api/app.py
+++ api/app.py
@@ -2,17 +2,6 @@ from flask import Flask
 from flask_mysql_connector import MySQL
 from flask_cors import CORS
 
-# # Initiating login manager options
-# login_manager = LoginManager()
-# login_manager.session_protection = "strong"
-# login_manager.login_view = "login"
-# login_manager.login_message_category = "info"
-#
-# #Initiating MySQL, migrations and bcrypt
-# db = MySQL()
-# migrate = Migrate()
-# bcrypt = Bcrypt()
-
 def create_app():
     app = Flask(__name__)
     CORS(app)
@@ -24,7 +13,7 @@ def create_app():
     app.config['MYSQL_HOST'] = 'db'
     app.config['MYSQL_PORT'] = '3306'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'Dd19995678!'
     app.config['MYSQL_DATABASE'] = 'xraylab'
     app.config['SECRET_KEY'] = 'secret key'
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
